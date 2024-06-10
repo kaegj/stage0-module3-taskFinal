1a2030c657673dc0855ca6aab3cf17f449aff0f0
kaegan <kkiefer4@student.umgc.edu>
2024-06-10 11:33:08 +0900

    Submission 1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int one = number % 10;
+        int two = (number / 10) % 10;
+        int three = (number / 100) % 10;
+        int four = (number / 1000) % 10;
+        int sum = one + two + three + four;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int one = number % 10;
+        int two = (number / 10) % 10;
+        int three = (number / 100) % 10;
+        int result = (one * 100) + two * 10 + three;
+        System.out.println(result);
     }
 }
